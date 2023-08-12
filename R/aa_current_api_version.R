fbfc4dd4b31b2218ff2c82f72c3b0038f3ceea84
momper14 <33731661+momper14@users.noreply.github.com>
2023-08-12 16:34:08 +0200

    Update aa_current_api_version.R

--- R/aa_current_api_version.R
+++ R/aa_current_api_version.R
@@ -3,7 +3,7 @@ aa_current_api_version <- function() {
   httr::add_headers(Accept = "text/plain")
   resp <- httr::GET(paste0(base_url, "/version"))
 
-  stop_for_status(resp)
+  httr::stop_for_status(resp)
 
   return(httr::content(resp))
 }
